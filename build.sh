74570ab45538605878b5aaf2f4ef4fc6ee4e5fbc
Alex Duly <apduly@syr.edu>
2025-09-22 20:26:20 -0400

    need to build clang too

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-docker compose down && docker rmi -f rabbitmq:3-management
+docker compose down #&& docker rmi -f rabbitmq:3-management
 docker build -t assemblage-gcc:base -f docker/gcc/Dockerfile .&&\
 docker build -t assemblage-gcc:default -f docker/gcc/gcc-default/Dockerfile .
+docker build -t assemblage-clang:base -f docker/clang/Dockerfile . && \
+docker build -t assemblage-clang:default -f docker/clang/clang-default/Dockerfile .
+
+[ -d binaries ] || mkdir binaries
+ 
