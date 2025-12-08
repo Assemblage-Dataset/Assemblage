8bcea87c5082d431388e4a47e410631df788ea5a
mhkerchen <121987287+mhkerchen@users.noreply.github.com>
2025-12-08 13:43:18 -0500

    re added separate alembic migration

--- backend/assemblage/data/initialize_database.py
+++ backend/assemblage/data/initialize_database.py
@@ -6,6 +6,7 @@ from assemblage.config import CoordinatorSettings
 # Basic setup of the DB on fresh installs/when it's deleted. 
 # Definitely could use some TLC (are two engines necessary?)
 # TODO: could definitely use fewer plain SQL queries, if desired
+# Will also break if we move from Postgres. 
 
 def conditional_init_db(settings: CoordinatorSettings):
 
@@ -15,7 +16,7 @@ def conditional_init_db(settings: CoordinatorSettings):
     s.db_name = "template1"
     engine = create_engine(s.databaseURL)
 
-    # Check for existence of assemblage DB, and create it if it doesn't exist
+    # Check for existence of assemblage DB from template1, and create it if it doesn't exist
     db_exists = False
     with engine.connect().execution_options(isolation_level="AUTOCOMMIT") as conn:
         db_exists = conn.execute(
@@ -27,8 +28,14 @@ def conditional_init_db(settings: CoordinatorSettings):
             logging.info(f"No database '{settings.db_name}' found. Automatically setting up database...")
             conn.execute(text(f"CREATE DATABASE {settings.db_name}"))
 
-    if not db_exists:
-        logging.info(f"No tables found. Running Alembic migration...")
+    assemblage_engine = create_engine(settings.databaseURL)
+    table_count = 0
+    with assemblage_engine.connect() as conn:
+
+        table_count = len( inspect(conn).get_table_names() )
+
+    if table_count == 0:
+        logging.info(f"No tables found in database {settings.db_name}. Running Alembic migration...")
         subprocess.run(
             ["alembic", "upgrade", "head"],
             check=True,
@@ -37,4 +44,5 @@ def conditional_init_db(settings: CoordinatorSettings):
         )
 
     engine.dispose()
+    assemblage_engine.dispose()
     logging.info(f"Database ready")
