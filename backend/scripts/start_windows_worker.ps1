644abe310dfaf5c25743e6a3b667c16ac1655c82
Alex Duly <alexduly41@gmail.com>
2025-12-22 17:50:57 +0000

    more configuration to enable dia2dump in container

--- backend/scripts/start_windows_worker.ps1
+++ backend/scripts/start_windows_worker.ps1
@@ -20,6 +20,27 @@ function Invoke-VcVars {
 
 # Invoke-VcVars -Version $Version
 Invoke-VcVars
+
+$env:PATH = "C:\tools\ctags;" + $env:PATH
+
+
+$diaDlls = @(
+    "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\DIA SDK\bin\amd64\msdia140.dll",
+    "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\DIA SDK\bin\x86\msdia140.dll"
+)
+
+foreach ($dll in $diaDlls) {
+    if (Test-Path $dll) {
+        Write-Host "Registering $dll ..."
+        Start-Process -FilePath "C:\Windows\System32\regsvr32.exe" `
+                      -ArgumentList "/s", "`"$dll`"" `
+                      -Wait -NoNewWindow
+    } else {
+        Write-Warning "DIA DLL not found: $dll"
+    }
+}
+
+
 python.exe C:\app\scripts\start_worker.py
 
 
